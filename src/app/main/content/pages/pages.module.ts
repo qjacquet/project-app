1e79473973916aafa2678d998711bb1ffdbc2593
Quentin JACQUET <qjacquet@smag.local>
2017-12-15 15:05:08 +0100

    .

--- src/app/main/content/pages/pages.module.ts
+++ src/app/main/content/pages/pages.module.ts
@@ -2,12 +2,14 @@ import { NgModule } from '@angular/core';
 
 import { LoginModule } from './auth/login/login.module';
 import { RegisterModule } from './auth/register/register.module';
+import { ProfileModule } from './profile/profile.module';
 
 @NgModule({
     imports: [
         // Auth
         LoginModule,
-        RegisterModule
+        RegisterModule,
+        ProfileModule
     ]
 })
 export class PagesModule
