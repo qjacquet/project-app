f0fad24736d51d5f729247f93f4a9babe83ce3b0
Quentin JACQUET <qjacquet@smag-group.com>
2018-04-17 15:31:14 +0200

    [WIP] init code coverage + karma + travisCI

--- README.md
+++ README.md
@@ -1,4 +1,7 @@
-# project-app
+[![Build Status](https://api.travis-ci.org/qjacquet/project-app.svg?branch=master)](https://travis-ci.org/qjacquet/project-app)
+[![codecov](https://codecov.io/gh/qjacquet/project-app/branch/master/graph/badge.svg)](https://codecov.io/gh/qjacquet/project-app)
+
+### project-app
 
 App built with Material Design and Angular
 
--- karma.conf.js
+++ karma.conf.js
@@ -10,24 +10,32 @@ module.exports = function (config) {
             require('karma-chrome-launcher'),
             require('karma-jasmine-html-reporter'),
             require('karma-coverage-istanbul-reporter'),
-            require('@angular/cli/plugins/karma')
+            require('@angular/cli/plugins/karma'),
+            require('karma-coverage'),
         ],
-        client:{
+        client: {
             clearContext: false // leave Jasmine Spec Runner output visible in browser
         },
         coverageIstanbulReporter: {
-            reports: [ 'html', 'lcovonly' ],
+            reports: ['html', 'lcovonly'],
             fixWebpackSourcePaths: true
+        },
+        coverageReporter: {
+            type: 'lcov',
+            dir: 'coverage/'
         },
         angularCli: {
             environment: 'dev'
         },
-        reporters: ['progress', 'kjhtml'],
+        reporters: ['progress', 'kjhtml', 'coverage'],
         port: 9876,
         colors: true,
         logLevel: config.LOG_INFO,
         autoWatch: true,
         browsers: ['Chrome'],
-        singleRun: false
+        singleRun: false,
+        preprocessors: {
+            'src/app/**/*.js': ['coverage']
+        },
     });
 };
